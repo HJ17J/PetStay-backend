432562c11716d5ddfa9920febc928ad9af80dcf1
HJ17J <hyeonj0130@gmail.com>
2024-04-24 10:21:52 +0900

    feat: prettierrc 수정

--- config/config.js
+++ config/config.js
@@ -1,19 +1,19 @@
 require("dotenv").config();
 
 const development = {
-    username: process.env.DB_USERNAME,
-    password: process.env.DB_PASSWORD,
-    database: process.env.DB_NAME,
-    host: process.env.DB_HOST,
-    dialect: "mysql",
+  username: process.env.DB_USERNAME,
+  password: process.env.DB_PASSWORD,
+  database: process.env.DB_NAME,
+  host: process.env.DB_HOST,
+  dialect: "mysql",
 };
 
 const production = {
-    username: process.env.RDS_USERNAME,
-    password: process.env.RDS_PASSWORD,
-    database: process.env.RDS_DBNAME,
-    host: process.env.RDS_HOST,
-    dialect: "mysql",
+  username: process.env.RDS_USERNAME,
+  password: process.env.RDS_PASSWORD,
+  database: process.env.RDS_DBNAME,
+  host: process.env.RDS_HOST,
+  dialect: "mysql",
 };
 
 module.exports = { development, production };
--- models/index.js
+++ models/index.js
@@ -5,9 +5,9 @@ const db = {};
 
 let config;
 if (process.env.NODE_ENV) {
-    config = require(__dirname + "/../config/config.js")[process.env.NODE_ENV];
+  config = require(__dirname + "/../config/config.js")[process.env.NODE_ENV];
 } else {
-    config = require(__dirname + "/../config/config.js")["development"];
+  config = require(__dirname + "/../config/config.js")["development"];
 }
 
 const sequelize = new Sequelize(config.database, config.username, config.password, config);
