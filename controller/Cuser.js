eb07ef7773ef549d6f419629bc717b0fabece0c8
hjh3933 <hkh3933@naver.com>
2024-04-24 16:43:31 +0900

    feat: profile조회와 수정

--- controller/Cuser.js
+++ controller/Cuser.js
@@ -9,12 +9,65 @@ exports.postProfile = async (req, res) => {
     });
     //type별 data전송
     if (userData.usertype === "user") {
-      res.send(userdata);
+      const resvData = await model.Reservations.findAll({
+        where: { useridx },
+      });
+      res.status(200).send({ userData, resvData });
     } else if (userData.usertype === "sitter") {
       const sitterData = await model.Sitters.findOne({
         where: { useridx },
       });
-      res.send(userData, sitterData);
+      const resvData = await model.Reservations.findAll({
+        where: { sitteridx: useridx },
+      });
+      res.status(200).send({ userData, sitterData, resvData });
+    }
+  } catch (err) {
+    console.log("err", err);
+    res.status(500).send("server err발생!!");
+  }
+};
+
+exports.updateProfile = async (req, res) => {
+  try {
+    const { useridx } = req.params;
+    const userData = await model.Users.findOne({ where: { useridx } });
+    const { userid, userpw, name, address, type, license, career, selfIntroduction, pay } =
+      req.body;
+
+    let updateFields = {
+      userid,
+      userpw,
+      name,
+      address,
+    };
+
+    // req.file이 존재하는 경우에만 img 필드 업데이트
+    if (req.file) {
+      updateFields.img = req.file.location;
+    }
+
+    await model.Users.update(updateFields, {
+      where: { useridx },
+    });
+
+    // type에 따라 수정작업
+    if (userData.usertype === "sitter") {
+      await model.Users.update(
+        {
+          type,
+          license,
+          career,
+          selfIntroduction,
+          pay,
+        },
+        {
+          where: { useridx },
+        }
+      );
+      res.status(200).send({ msg: "펫시터 회원수정 완료" });
+    } else {
+      res.status(200).send({ msg: "일반 회원수정 완료" });
     }
   } catch (err) {
     console.log("err", err);
