8f5ec14d2fd758ed2ecab88753c0c9cdea38c206
yhs0329 <scar0329@naver.com>
2024-04-26 16:07:36 +0900

    feat:예약 확정, 거절 취소 기능 추가

--- routes/index.js
+++ routes/index.js
@@ -42,6 +42,10 @@ router.post("/join", Cuser.postJoin);
 router.get("/login", Cuser.getLogin);
 router.post("/login", Cuser.postLogin);
 router.post("/logout", Cuser.postLogout);
+//예약확정,거절, 취소 - 형석
+router.patch("/reservation/:resvidx/confirm", Creservation.confirmReservation);
+router.patch("/reservation/:resvidx/refused", Creservation.refusedReservation);
+router.delete("/reservation/:resvidx/delete", Creservation.deleteReservation);
 
 //회원정보 조회
 router.post("/profile/:useridx", Cuser.postProfile);
