77af954c9df2fc93b4c56f661184f99a8ef7abd0
yhs0329 <scar0329@naver.com>
2024-05-05 11:51:51 +0900

    feat:소셜 로그인 카카오 기능 추가

--- config/passport.js
+++ config/passport.js
@@ -1,9 +1,42 @@
 const passport = require("passport");
 const GoogleStrategy = require("passport-google-oauth20").Strategy;
+const KakaoStrategy = require("passport").Strategy;
 const Users = require("../models/Users").Users;
 const PORT = process.env.PORT;
 const axios = require("axios");
 
+// 카카오 로그인 부분
+passport.use(
+  new KakaoStrategy(
+    {
+      clientID: process.env.KAKAO_CLIENT_ID,
+      clientSecret: process.env.KAKAO_CLIENT_SECRET,
+      callbackURL: `http://localhost:${PORT}/auth/kakao/callback`,
+    },
+    async (accessToken, refreshToken, profile, done) => {
+      console.log(profile);
+      try {
+        const user = await Users.findOrCreate({
+          where: { userid: profile.id.toString() }, // 카카오 ID를 문자열로 변환
+          defaults: {
+            userid: profile.id.toString(), // 카카오 ID
+            userpw: null, // 비밀번호는 null
+            name: profile.username, // 카카오 닉네임
+            address: "비어있음", //
+            img: profile._json.properties.profile_image, // 프로필 이미지 URL
+            usertype: "user",
+          },
+        });
+        return done(null, user[0]);
+      } catch (error) {
+        console.log("카카오 로그인 중 에러 발생", error);
+        return done(error);
+      }
+    }
+  )
+);
+
+// 구글 로그인 부분
 passport.use(
   new GoogleStrategy(
     {
@@ -44,11 +77,11 @@ passport.use(
 
 // Passport 세션 설정
 passport.serializeUser((user, done) => {
-  done(null, user.id);
+  done(null, user.useridx);
 });
 
 passport.deserializeUser((id, done) => {
-  Users.findById(id)
+  Users.findByPk(id)
     .then((user) => {
       done(null, user);
     })
--- routes/auth.js
+++ routes/auth.js
@@ -2,6 +2,28 @@ const express = require("express");
 const passport = require("passport");
 const router = express.Router();
 
+// kakao 로그인 라우트
+router.get("/kakao", passport.authenticate("kakao"));
+
+// kakao 로그인 콜백 라우트
+router.get(
+  "kakao/callback",
+  passport.authenticate("kakao", { failureRedirect: "/login" }),
+  function (req, res) {
+    res.json({
+      success: true,
+      message: "로그인 성공",
+      user: {
+        id: req.user.useridx, // 데이터베이스의 기본 키
+        userid: req.user.userid, // 사용자 식별자
+        name: req.user.name,
+        email: req.user.email, // 이메일은 선택적으로 카카오에서 받을 수 있음
+        img: req.user.img,
+      },
+    });
+  }
+);
+
 // google 로그인 라우트
 router.get("/google", passport.authenticate("google", { scope: ["profile", "email"] }));
 // google 로그인 콜백 라우트
